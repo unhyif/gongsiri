90efc1a67cf4ebb220e342bdd5c33892071f5257
unhyif <ang21icjh@gmail.com>
2024-04-22 12:35:27 +0900

    feat: scrap 결과에서 주석 제거

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,22 @@
 import puppeteer from 'puppeteer';
 
+export const removeComments = (html: string) => {
+  return html.replace(/<!--[\s\S]*?-->/g, '');
+};
+
 export const scrapMainContent = async (url: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle0' });
 
   const result = await page.evaluate(() => {
-    const selectorsToRemove = ['nav', 'header', 'footer', 'img', 'script'];
+    const selectorsToRemove = ['script', 'nav', 'header', 'footer', 'img'];
     selectorsToRemove.forEach(selector => {
       const elements = document.querySelectorAll(selector);
       elements.forEach(element => element.remove());
     });
-    return document.body.innerHTML;
+
+    return removeComments(document.body.innerHTML);
   });
 
   await browser.close();
