a139daeaf0b91eda15370edde2fe5fa9f8a57ce1
unhyif <ang21icjh@gmail.com>
2024-04-22 21:48:37 +0900

    feat: 주택 리스트 scrapper에 페이지 이동 적용

--- src/scripts/houseListScrapper.mjs
+++ src/scripts/houseListScrapper.mjs
@@ -3,27 +3,54 @@ import puppeteer from 'puppeteer';
 const HOUSE_LIST_ENTRY_URL =
   'https://soco.seoul.go.kr/youth/pgm/home/yohome/list.do?menuNo=400002';
 
+const getNumOfTotalPages = () =>
+  Number(document.body.querySelector('#totPage').innerText.split('/')[1]);
+
+const getHouses = () => {
+  const houses = [];
+
+  const rows = document.body.querySelectorAll('#cohomeForm tr');
+  rows.forEach(row => {
+    const cells = row.querySelectorAll('td');
+    const house = {
+      id: Number(cells[0].innerText.trim()),
+      area: cells[2].innerText.trim(),
+      name: cells[3].innerText.trim(),
+    };
+    houses.push(house);
+  });
+
+  return houses;
+};
+
+const goNext = async (page, nextPageIndex) => {
+  return await Promise.all([
+    page.click('.paginate button.next'),
+    page.waitForFunction(
+      nextPageIndex =>
+        document.body.querySelector('.paginate button.on').innerText ===
+        String(nextPageIndex),
+      {},
+      nextPageIndex
+    ),
+  ]);
+};
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(HOUSE_LIST_ENTRY_URL, { waitUntil: 'networkidle0' });
 
-  const result = await page.evaluate(() => {
-    const houses = [];
+  const numOfTotalPages = await page.evaluate(getNumOfTotalPages);
+  let result = [];
 
-    const rows = document.body.querySelectorAll('#cohomeForm tr');
-    rows.forEach(row => {
-      const cells = row.querySelectorAll('td');
-      const house = {
-        id: Number(cells[0].textContent ?? -1),
-        area: (cells[2].textContent ?? '').trim(),
-        name: (cells[3].textContent ?? '').trim(),
-      };
-      houses.push(house);
-    });
+  for (let currentPage = 1; currentPage <= numOfTotalPages; currentPage++) {
+    const additionalResult = await page.evaluate(getHouses);
+    result = result.concat(additionalResult);
 
-    return houses;
-  });
+    if (currentPage === numOfTotalPages) break;
+    await goNext(page, currentPage + 1);
+  }
 
   await browser.close();
 
