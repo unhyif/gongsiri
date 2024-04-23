10e812a00898e5144f71604845a6480ae15b7969
unhyif <ang21icjh@gmail.com>
2024-04-23 22:01:27 +0900

    feat: 주택 공지사항 메뉴 링크 크롤링

--- src/scripts/houseAnnouncementUrlCrawler.mjs
+++ src/scripts/houseAnnouncementUrlCrawler.mjs
@@ -1,22 +1,38 @@
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import dotenv from 'dotenv';
 import fs from 'fs';
 import puppeteer from 'puppeteer';
 
-const scrapStaticUrls = async url => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
+dotenv.config({ path: '.env' });
 
-  const result = await page.evaluate(() => {
-    const links = document.body.querySelectorAll('a');
-    return Array.from(links).map(link => ({
+const scrapStaticUrls = () => {
+  const links = document.body.querySelectorAll('a');
+  return JSON.stringify(
+    Array.from(links).map(link => ({
       href: link.href,
       text: link.textContent,
-    }));
+    }))
+  );
+};
+
+const getHouseAnnouncementUrl = async input => {
+  const model = new ChatOpenAI({
+    cache: true,
   });
-
-  await browser.close();
-
-  return result;
+  const parser = new StringOutputParser();
+  // TODO: 수정
+  const systemPrompt = `공지사항 링크를 찾아줘.
+  링크만 출력해야 해. 예를 들어 링크가 "https://www.abc.com" 이면, 답변은 https://www.abc.com 여야 해.
+  링크를 찾지 못했다면 아무것도 출력하지마.`;
+  const prompt = ChatPromptTemplate.fromMessages([
+    ['system', systemPrompt],
+    ['user', '{input}'],
+  ]);
+  const chain = prompt.pipe(model).pipe(parser);
+  const res = await chain.invoke({ input });
+  return res;
 };
 
 export const getHouseAnnouncementUrls = () => {
@@ -27,10 +43,17 @@ export const getHouseAnnouncementUrls = () => {
     const houses = JSON.parse(data);
 
     for (const house of houses) {
-      // scrapStaticUrls
-      // prompt
-      // house.annoucementUrl =
+      try {
+        await page.goto(house.url);
+        const urls = await page.evaluate(scrapStaticUrls);
+        house.announcementUrl = await getHouseAnnouncementUrl(urls);
+      } catch (e) {
+        console.log(house, e);
+        house.announcementUrl = null;
+      }
     }
+
+    await browser.close();
 
     fs.writeFile(
       './src/data/houseList.json',
