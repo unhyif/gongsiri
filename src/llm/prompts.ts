ebf54d179aff83de06bc509a7a7fd5377af6b51c
unhyif <ang21icjh@gmail.com>
2024-04-25 19:55:09 +0900

    chore: 주택 리스트 정보 추가

--- src/llm/prompts.ts
+++ src/llm/prompts.ts
@@ -13,7 +13,7 @@ export const latestAnnouncementHumanPrompt = `주어지는 context는 공지사�
 </context>
 
 <question>
-HTML 페이지에 포함된 공지사항 중, 가장 최근 공지사항의 제목과 날짜를 찾아주세요.
+HTML 페이지에 포함된 공지사항 중, 가장 최근에 작성된 공지사항의 제목과 날짜를 찾아주세요.
 - 찾은 제목과 날짜를 그대로 제공하세요. 그 외 다른 내용은 추가하지 마세요.
 - 공지사항 목록은 'table' 태그 내에 위치해 있을 가능성이 높습니다.
 </question>
