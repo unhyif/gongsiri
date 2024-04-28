85491414ea86e3b0038f71ddf3dccff547ac1a1f
unhyif <ang21icjh@gmail.com>
2024-04-28 17:32:39 +0900

    chore: ISR test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,15 @@ import { House } from '../types';
 
 export default async function Home() {
   const res = await fetch(process.env.API_BASE_URL + '/houses', {
-    next: { revalidate: 1800 },
+    next: { revalidate: 10 },
   });
   const { data, updatedAt }: { data: House[]; updatedAt: number } =
     await res.json();
 
   return (
     <main>
-      최근 업데이트: {new Date(updatedAt).toString()}
+      최근 업데이트:{' '}
+      {new Date(Date.now()).toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' })}
       <ul style={{ padding: '24px' }}>
         {data.map(house => {
           const { id, area, name, announcementUrl, latestAnnouncement } = house;
