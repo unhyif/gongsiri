a31f5425db115b46f1e4e36814d7f1caebbcac3e
unhyif <ang21icjh@gmail.com>
2024-04-30 18:10:54 +0900

    chore: ISR 중지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,38 @@
-import { House } from '../types';
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
 
 export default async function Home() {
-  const res = await fetch(process.env.API_BASE_URL + '/houses', {
-    next: { revalidate: 1800 },
+  const client = new DynamoDBClient({});
+  const docClient = DynamoDBDocumentClient.from(client);
+
+  // TODO: 함수 분리
+
+  const houseListScanCommand = new ScanCommand({
+    TableName: process.env.HOUSE_TABLE,
   });
-  const { data, updatedAt }: { data: House[]; updatedAt: number } =
-    await res.json();
+  const { Items: houses = [] } = await docClient.send(houseListScanCommand);
+
+  const houseListUpdatedAtGetCommand = new GetItemCommand({
+    TableName: process.env.EXTRA_DATA_TABLE,
+    Key: {
+      name: {
+        S: 'updatedAt',
+      },
+    },
+    AttributesToGet: ['value'],
+  });
+
+  const { Item: updatedAtObj } = await docClient.send(
+    houseListUpdatedAtGetCommand
+  );
+  const updatedAt = Number(updatedAtObj?.value.N);
 
   return (
     <main>
       최근 업데이트:{' '}
       {new Date(updatedAt).toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' })}
       <ul style={{ padding: '24px' }}>
-        {data.map(house => {
+        {houses.map(house => {
           const { id, area, name, announcementUrl, latestAnnouncement } = house;
           return (
             <li key={id}>
