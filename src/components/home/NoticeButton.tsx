57400e948c764c5e496f6588641b68533085ee7d
unhyif <ang21icjh@gmail.com>
2024-08-03 18:57:07 +0900

    fix: 공지 버튼 이름 수정

--- src/components/home/NoticeButton.tsx
+++ src/components/home/NoticeButton.tsx
@@ -1,17 +1,8 @@
 'use client';
 
-import { formatInTimeZone } from 'date-fns-tz';
-import { ko } from 'date-fns/locale';
-
 const TEXT =
   '- 주택 목록을 최신 공지순으로 정렬하였습니다.\n- 최근 Elyes 계열 주택의 공지가 수집되지 않는 문제가 발생하여, 해당 주택을 임시적으로 목록 상단에 배치하였습니다.';
 
 export const NoticeButton = () => (
-  <button onClick={() => alert(TEXT)}>
-    📣 공지 (
-    {formatInTimeZone(new Date(), 'Asia/Seoul', 'MM.dd', {
-      locale: ko,
-    })}
-    )
-  </button>
+  <button onClick={() => alert(TEXT)}>📣 공지 (08.03)</button>
 );
